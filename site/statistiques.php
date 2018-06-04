8ff2bc55c46eec047bfa752b7ef9952dbe93e4e2
Josslin Varon <josslinvaron@gmail.com>
2018-06-04 22:15:01 +0200

    Ajout de la page statistique + lien

---
+++ site/statistiques.php
@@ -0,0 +1,17 @@
+<?php require('../functions/verifConnexion.php')?>
+<!doctype html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <title>Medical Care - Statistiques</title>
+      <?php require('../css/style.php');  ?>
+
+  </head>
+
+  <body>
+      <?php require('../view/navBar.html'); ?>
+      <main>
+        <h1>Fonctionnalité à venir</h1>
+      </main>
+  </body>
+</html>
